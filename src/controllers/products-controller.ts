d5abef9d2442e518e85453b35ee52eab7d236a1c
EsleySaab <esleysantana.sa@gmail.com>
2024-11-27 19:00:55 -0300

    creating the table of tables

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express"
 import { AppError } from "@/utils/AppError"
-import { knex } from "@/database/migrations/knex"
+import { knex } from "@/database/knex"
 import { z } from "zod"
 
 class ProductController {
