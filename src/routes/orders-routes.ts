067439f2ffd020c8b0ef853d67fce54343eb7b70
EsleySaab <esleysantana.sa@gmail.com>
2024-11-28 16:13:45 -0300

    listing orders by table

--- src/routes/orders-routes.ts
+++ src/routes/orders-routes.ts
@@ -7,4 +7,6 @@ const ordersController = new OrdersController()
 
 ordersRoutes.post("/", ordersController.create)
 
+ordersRoutes.get("/table-session/:table_session_id", ordersController.index)
+
 export { ordersRoutes }
